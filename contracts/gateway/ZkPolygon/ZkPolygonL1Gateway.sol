18d25bf29da1c1a8decfbafc3d58c6a1eb5a1276
zwjkf <503755414@qq.com>
2024-02-01 17:04:42 +0800

    ZkPolygon

---
+++ contracts/gateway/ZkPolygon/ZkPolygonL1Gateway.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.0;
+
+import {IZkPolygon} from "../../interfaces/ZkPolygon/IZkPolygon.sol";
+import {IArbitrator} from "../../interfaces/IArbitrator.sol";
+import {L1BaseGateway} from "../L1BaseGateway.sol";
+import {IZkPolygonL1Gateway} from "../../interfaces/ZkPolygon/IZkPolygonL1Gateway.sol";
+import {IZkPolygonL2Gateway} from "../../interfaces/ZkPolygon/IZkPolygonL2Gateway.sol";
+import {BaseGateway} from "../BaseGateway.sol";
+
+contract ZkPolygonL1Gateway is IZkPolygonL1Gateway, L1BaseGateway, BaseGateway {
+    /// @notice ZkPolygon message service on local chain
+    IZkPolygon public messageService;
+
+
+    /// @dev Receive eth from ZkPolygon canonical bridge
+    receive() external payable {
+    }
+
+    function initialize(IArbitrator _arbitrator, IZkPolygon _messageService) external initializer {
+        __L1BaseGateway_init(_arbitrator);
+        __BaseGateway_init();
+
+        messageService = _messageService;
+    }
+
+    function sendMessage(uint256 _value, bytes memory _callData, bytes memory _adapterParams) external payable onlyArbitrator {
+        require(msg.value == _value, "Invalid value");
+        messageService.bridgeMessage{value: msg.value}(
+            1,
+            remoteGateway,
+            true,
+            _callData
+        );
+    }
+
+    function finalizeMessage(uint256 _value, bytes memory _callData) external payable nonReentrant {
+        // no fee
+        require(msg.value == _value, "Invalid value from canonical message service");
+
+        arbitrator.receiveMessage{value: msg.value}(_value, _callData);
+    }
+}
---
+++ contracts/gateway/ZkPolygon/ZkPolygonL2Gateway.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.0;
+
+import {IZkPolygon} from "../../interfaces/ZkPolygon/IZkPolygon.sol";
+import {IZkPolygonL2Gateway} from "../../interfaces/ZkPolygon/IZkPolygonL2Gateway.sol";
+import {IZkLink} from "../../interfaces/IZkLink.sol";
+import {L2BaseGateway} from "../L2BaseGateway.sol";
+import {BaseGateway} from "../BaseGateway.sol";
+
+contract ZkPolygonL2Gateway is IZkPolygonL2Gateway, L2BaseGateway, BaseGateway {
+    /// @notice ZkPolygon message service on local chain
+    IZkPolygon public messageService;
+    /// @dev Modifier to make sure the original sender is messageService on remote chain.
+    modifier onlyMessageService() {
+        require(msg.sender == address(messageService), "Not remote gateway");
+        _;
+    }
+    function initialize(IZkLink _zkLink, IZkPolygon _messageService) external initializer {
+        __L2BaseGateway_init(_zkLink);
+        __BaseGateway_init();
+
+        messageService = _messageService;
+    }
+
+    function sendMessage(uint256 _value, bytes memory _callData) external payable onlyZkLink {
+        require(msg.value == _value, "Invalid value");
+        messageService.bridgeMessage{value: msg.value}(
+            1,
+            remoteGateway,
+            true,
+            _callData
+        );
+    }
+
+    function finalizeMessage(uint256 _value, bytes memory _callData) external payable  onlyMessageService {
+        require(msg.value == _value, "Invalid value from canonical message service");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success,) = address(zkLink).call{value: _value}(_callData);
+        require(success, "Call zkLink failed");
+    }
+}
