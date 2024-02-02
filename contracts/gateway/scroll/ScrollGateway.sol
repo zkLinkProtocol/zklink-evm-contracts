c634c0542fd6be6264049932b7992d685c54be68
zkJoaquin <158016937+zkJoaquin@users.noreply.github.com>
2024-02-02 10:22:43 +0800

    fix scrollGateway bug
    * fix scrollGateway bug
    
    * fix inheritance issues

---
+++ contracts/gateway/scroll/ScrollGateway.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.0;
+
+import {IScrollMessenger} from "../../interfaces/scroll/IScrollMessenger.sol";
+import {IScrollGateway} from "../../interfaces/scroll/IScrollGateway.sol";
+import {BaseGateway} from "../BaseGateway.sol";
+
+abstract contract ScrollGateway is BaseGateway, IScrollGateway {
+    /// @notice Linea message service on local chain
+    IScrollMessenger public messageService;
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[49] private __gap;
+
+    /// @dev Modifier to make sure the caller is the known message service.
+    modifier onlyMessageService() {
+        require(msg.sender == address(messageService), "Not message service");
+        _;
+    }
+
+    function __ScrollGateway_init(IScrollMessenger _messageService) internal onlyInitializing {
+        __BaseGateway_init();
+        __ScrollGateway_init_unchained(_messageService);
+    }
+
+    function __ScrollGateway_init_unchained(IScrollMessenger _messageService) internal onlyInitializing {
+        messageService = _messageService;
+    }
+}
