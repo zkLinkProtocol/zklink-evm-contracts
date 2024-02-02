b49dd4f244a3345f17a98592a98b74506ba08bcb
zwjkf <503755414@qq.com>
2024-02-02 12:15:19 +0800

    Integrate ZkPolygonGateway interface

--- contracts/interfaces/zkpolygon/IZkPolygonGateway.sol
+++ contracts/interfaces/zkpolygon/IZkPolygonGateway.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.0;
 
-interface IZkPolygonL2Gateway {
+interface IZkPolygonGateway {
     function claimMessageCallback(uint256 _value, bytes memory _callData) external payable;
 }
