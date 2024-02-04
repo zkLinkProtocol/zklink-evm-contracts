35ceac2501d64d45aaed145c9a020e49445b5e6b
zkbenny <benny@zklink.org>
2024-02-04 10:46:45 +0800

    add arbitrum example

---
+++ examples/arbitrum/hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const BaseConfig = require("../../hardhat.base.config");
+
+module.exports = Object.assign({}, BaseConfig, {
+    paths: {
+        cache: "../../cache",
+        artifacts: "../../artifacts"
+    }
+})
