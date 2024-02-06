2a4f0bace829baa12d8fe67f81723017247cf301
zkJoaquin <joaquin@zklink.org>
2024-02-06 19:30:36 +0800

    add changeFeeParams case for scroll/zksync

--- examples/scroll/hardhat.config.js
+++ examples/scroll/hardhat.config.js
@@ -3,6 +3,7 @@ require('./scripts/1_initConfig');
 require('./scripts/2_syncL2Requests');
 require('./scripts/3_syncBatchRoot');
 require('./scripts/4_setValidator');
+require('./scripts/5_changeFeeParams');
 
 const BaseConfig = require('../../hardhat.base.config');
 
