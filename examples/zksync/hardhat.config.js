f6383e65c8845106f4e8066b883d45fd3d99ba5a
zkJoaquin <joaquin@zklink.org>
2024-02-06 16:15:10 +0800

    add setValidator case for zksync

--- examples/zksync/hardhat.config.js
+++ examples/zksync/hardhat.config.js
@@ -2,6 +2,7 @@ require('@nomicfoundation/hardhat-toolbox');
 require('@matterlabs/hardhat-zksync-verify');
 require('./scripts/syncL2Requests');
 require('./scripts/syncBatchRoot');
+require('./scripts/setValidator');
 
 const BaseConfig = require('../../hardhat.base.config');
 
