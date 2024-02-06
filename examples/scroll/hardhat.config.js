68a2dacb4ff935e32804986f49d8b08ed2b1ae69
zkBenny <85873552+zkbenny@users.noreply.github.com>
2024-02-06 15:37:02 +0800

    add setValidator for scroll (#26)
    * add setValidator case for scroll
    
    * fix comment
    
    * add setSecondaryGateway script (#24)
    
    * add setSecondaryGateway script
    
    * fix solidity version
    
    * update set validator
    
    ---------
    
    Co-authored-by: zkJoaquin <joaquin@zklink.org>

--- examples/scroll/hardhat.config.js
+++ examples/scroll/hardhat.config.js
@@ -2,6 +2,7 @@ require('@nomiclabs/hardhat-ethers');
 require('./scripts/1_initConfig');
 require('./scripts/2_syncL2Requests');
 require('./scripts/3_syncBatchRoot');
+require('./scripts/4_setValidator');
 
 const BaseConfig = require('../../hardhat.base.config');
 
