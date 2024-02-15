2d65f0ef243f401aaf2f332eecb77b96f9eaa78b
zkJoaquin <joaquin@zklink.org>
2024-02-15 16:09:55 +0800

    modify the filename

--- examples/scroll/hardhat.config.js
+++ examples/scroll/hardhat.config.js
@@ -1,9 +1,8 @@
 require('@nomicfoundation/hardhat-toolbox');
-require('./scripts/1_initConfig');
-require('./scripts/2_syncL2Requests');
-require('./scripts/3_syncBatchRoot');
-require('./scripts/4_setValidator');
-require('./scripts/5_changeFeeParams');
+require('./scripts/syncL2Requests');
+require('./scripts/syncBatchRoot');
+require('./scripts/setValidator');
+require('./scripts/changeFeeParams');
 
 const BaseConfig = require('../../hardhat.base.config');
 
