9bed074348f3953092abbae689129c58210a58dc
zkbenny <benny@zklink.org>
2024-03-21 21:04:25 +0800

    fix: abdk-cvf-13

--- contracts/interfaces/zksync/IL2Messenger.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-
-pragma solidity ^0.8.0;
-
-/// @author Matter Labs
-interface IL2Messenger {
-    function sendToL1(bytes memory _message) external returns (bytes32);
-}
