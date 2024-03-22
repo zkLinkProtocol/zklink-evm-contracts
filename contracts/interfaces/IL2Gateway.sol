8d991a615d8c4fbbef2f2eafce75077416a0623f
zkbenny <benny@zklink.org>
2024-03-22 11:38:42 +0800

    fix: abdk-cvf-43

--- contracts/interfaces/IL2Gateway.sol
+++ contracts/interfaces/IL2Gateway.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 import {IGateway} from "./IGateway.sol";
 
 interface IL2Gateway is IGateway {
+    /// @notice Emit when sending a message
+    event L2GatewayMessageSent(uint256 value, bytes callData);
+
     /// @notice Send message to remote gateway
     /// @param _value The msg value
     /// @param _callData The call data
