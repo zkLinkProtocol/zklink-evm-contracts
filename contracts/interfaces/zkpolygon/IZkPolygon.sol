44b9cc8435069841262658870e3f8f1523d46bea
zkbenny <benny@zklink.org>
2024-03-22 09:49:05 +0800

    fix: abdk-cvf-14

--- contracts/interfaces/zkpolygon/IZkPolygon.sol
+++ contracts/interfaces/zkpolygon/IZkPolygon.sol
@@ -2,45 +2,10 @@
 pragma solidity ^0.8.0;
 
 interface IZkPolygon {
-    function bridgeAsset(
-        uint32 destinationNetwork,
-        address destinationAddress,
-        uint256 amount,
-        address token,
-        bool forceUpdateGlobalExitRoot,
-        bytes calldata permitData
-    ) external payable;
-
     function bridgeMessage(
         uint32 destinationNetwork,
         address destinationAddress,
         bool forceUpdateGlobalExitRoot,
         bytes calldata metadata
     ) external payable;
-
-    function claimAsset(
-        bytes32[32] calldata smtProof,
-        uint32 index,
-        bytes32 mainnetExitRoot,
-        bytes32 rollupExitRoot,
-        uint32 originNetwork,
-        address originTokenAddress,
-        uint32 destinationNetwork,
-        address destinationAddress,
-        uint256 amount,
-        bytes calldata metadata
-    ) external;
-
-    function claimMessage(
-        bytes32[32] calldata smtProof,
-        uint32 index,
-        bytes32 mainnetExitRoot,
-        bytes32 rollupExitRoot,
-        uint32 originNetwork,
-        address originAddress,
-        uint32 destinationNetwork,
-        address destinationAddress,
-        uint256 amount,
-        bytes calldata metadata
-    ) external;
 }
