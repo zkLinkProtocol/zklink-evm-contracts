d8e4c1c5379d4df7dec3e641e9a4b4b96215892c
zkbenny <benny@zklink.org>
2024-09-10 19:38:09 +0800

    add gas price arg

--- examples/zklinkNova/scripts/sendRawTx.js
+++ examples/zklinkNova/scripts/sendRawTx.js
@@ -7,10 +7,12 @@ task('sendRawTx', 'Send raw tx to nova')
   .addParam('to', 'The to address', undefined, types.string)
   .addParam('data', 'The call data', undefined, types.string)
   .addOptionalParam('value', 'The call value(unit: ether)', '0', types.string)
+  .addOptionalParam('gasPrice', 'The gas price(unit: gwei)', '0', types.string)
   .setAction(async taskArgs => {
     const to = taskArgs.to;
     const data = taskArgs.data;
     const value = ethers.parseEther(taskArgs.value);
+    const gasPrice = ethers.parseUnits(taskArgs.gasPrice, 'gwei');
 
     const provider = new ethers.JsonRpcProvider(process.env.ZKLINK_NOVA_RPC);
     const wallet = new ethers.Wallet(process.env.DEVNET_PRIVKEY, provider);
@@ -18,6 +20,7 @@ task('sendRawTx', 'Send raw tx to nova')
       to,
       data,
       value,
+      gasPrice
     });
     console.log(`The tx hash: ${tx.hash}`);
     const txReceipt = await tx.wait(1);
